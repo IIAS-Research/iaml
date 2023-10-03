30e02788ba955ec0819490b5023f91c240ef7c69
Rudy Merieux <rmerieux@chu-reims.fr>
2023-10-03 17:27:47 +0200

    [WIP] Ci test

--- python_project/src/perf_logger/perf_logger.py
+++ python_project/src/perf_logger/perf_logger.py
@@ -52,7 +52,7 @@ results.to_csv(history_path, index=False)
 # Create graph
 # df_plot = results.pivot_table(index='commit_id', columns='dataset_name', values='perf')
 # plot = sns.lineplot(data=df_plot)
-df_plot = results.sort_values(by=['date'])
+# df_plot = results.sort_values(by=['date'])
 plot = sns.lineplot(data=df_plot, x="commit_id", y="perf", hue="dataset_name")
 sns.move_legend(plot, "upper left", bbox_to_anchor=(1, 1))
 plot.set(ylim = (.5,1))
