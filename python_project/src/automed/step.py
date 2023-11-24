76ce1accb54c08f98543dd15f5a7609fb8fc9e10
Robin Bourachot <rbourachot@chu-reims.fr>
2023-11-24 17:02:03 +0100

    [ADD] rich string of step name/conf

--- python_project/src/automed/step.py
+++ python_project/src/automed/step.py
@@ -1,6 +1,7 @@
 from .output import Output, Input
 from .dataset import Dataset
 from .stack import Stack
+from .logger import logger
 
 from copy import deepcopy
 
@@ -213,6 +214,23 @@ class Step:
             'children': []
         }
     
+
+    def conf_to_rich_str_list(self):
+        conf = [ f'{name}={conf["value"]}' for name, conf in self.configurations[0].items() ]
+        
+        return conf
+
+
+    def to_rich_str(self):
+        conf = self.conf_to_rich_str_list()
+
+        if len(conf) > 0:
+            name = f'[b]{self.__class__.__name__}[/] ({", ".join(conf)})'
+        else:
+            name = f'[b]{self.__class__.__name__}[/]'
+        
+        return name
+    
     ############
     # Priorize #
     ############
@@ -345,7 +363,11 @@ def runner(func):
         result:list[Output] = []
         for current in self.configurations:
             self.current_configuration = current
-            print("# RUN #", self, self.resume_configuration())
+
+            # only print "parent" steps to reduce logs
+            if hasattr(self, 'step') or hasattr(self, 'steps'):
+                logger.log(f'running step: {self.to_rich_str()}')
+            
             for input in inputs:
                 
                 # Destroyer will stop Step run if results are not good enough
