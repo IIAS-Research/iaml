b6e456801c253803757c5144c83fc874d7abd944
Robin Bourachot <rbourachot@chu-reims.fr>
2023-11-24 17:00:50 +0100

    [ADD] global logger

---
+++ python_project/src/automed/logger.py
@@ -0,0 +1,17 @@
+import rich.console
+import rich.progress
+
+
+class Logger:
+    def __init__(self, quiet=False) -> None:
+        self.console = rich.console.Console()
+        self.progress = rich.progress.Progress(console=self.console)
+        self.quiet = quiet
+
+
+    def log(self, *text: list[str]):
+        if not self.quiet:
+            self.console.log(*text)
+
+
+logger = Logger()
