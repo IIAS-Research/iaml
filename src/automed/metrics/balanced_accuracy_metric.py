3f2956a02afacff9f6852a34b456a7b594c88a78
Rudy Merieux <rmerieux@chu-reims.fr>
2024-02-26 14:10:16 +0100

    [FIX] Balanced Accuracy is good same on inbalanced

--- src/automed/metrics/balanced_accuracy_metric.py
+++ src/automed/metrics/balanced_accuracy_metric.py
@@ -14,20 +14,12 @@ class BalancedAccuracyMetric(Metric):
     def __str__(self):
         return 'balanced_accuracy'
     
-    # Get one label (numpy.array or pd.series) and return false if classes is balanced
-    def __is_balanced(self, y):
-        class_count = Counter(y)
-        total_samples = y.shape[0]
-        ideal_count = total_samples/len(class_count)
-        threshold = 0.20 * ideal_count
-        return not(any(abs(count - ideal_count) > threshold for count in class_count.values()))
-    
     def suitable(self, y):
         if not isinstance(y, pd.DataFrame):
             y = pd.DataFrame(y)
         
         for column in y.columns:
-            if not(type_of_target(y[column]) in ['binary', 'multiclass'] and self.__is_balanced(y[column])):
+            if type_of_target(y[column]) in ['binary', 'multiclass']:
                 return True
         
         return False
