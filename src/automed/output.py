[FIX] better shap values computation time

src/automed/output.py
@@ -4,9 +4,9 @@ Output (and Input alias) is used to exchange data between Steps
 from typing import TYPE_CHECKING
 
 from copy import copy
+import numpy as np
 import pandas as pd
 import shap
-import warnings
 
 from .auto_pipeline import AutoPipeline
 from .dataset import Dataset
@@ -228,7 +228,7 @@ class Output:
             
         return current_compute
     
-    def explain_model(self, X: 'pd.DataFrame', max_evals: int = 'auto'):
+    def explain_model(self, X: 'pd.DataFrame', nsamples: int = 20):
         """
         Explains the model by computing SHAP values on the fitted model.
         Uses the train set as the masker, and the provided set as
@@ -236,8 +236,11 @@ class Output:
 
         Args:
             X (DataFrame): Prediction set to compute SHAP values for.
-            max_evals (int): Max number of SHAP evaluations to do on
-                the dataset.
+            nsamples (int, optional): Number of samples to pick from
+                the masker to pick feature data from for each row in
+                the provided prediction dataset. More samples means
+                more accurate SHAP values and longer computing times.
+                Defaults to 20.
 
         Returns:
             Explanation: Model explanation, with an overview of the
@@ -252,16 +255,15 @@ class Output:
             return self.pipeline.predict_proba(pd.DataFrame(pred_data, columns=X.columns))[:, 1]
 
         # explainer = shap.Explainer(lambda x: model.predict_proba(x)[:, 1], med)
-        n = 68
         explainer = shap.KernelExplainer(p, X)
-        shap_values = explainer.shap_values(X, nsamples=n)
+        shap_values = explainer.shap_values(X, nsamples=nsamples)
 
-        import numpy as np
         shap_explanation = shap.Explanation(
             shap_values,
             base_values=np.tile(explainer.expected_value, (shap_values.shape[0], 1)),
-            data=X,
-            feature_names=X.columns)
+            data=X.to_numpy(),
+            feature_names=X.columns.to_list(),
+            output_names=X.columns.to_list())
 
         return Explanation(step, None, None, shap_explanation)
     