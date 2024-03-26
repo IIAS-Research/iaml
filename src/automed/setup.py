dbca011ff15d10aa5cfce13f73d2ecf1eb0625b5
Rudy Merieux <rmerieux@chu-reims.fr>
2024-03-26 22:25:13 +0100

    [REFA] Package setup

---
+++ src/automed/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='automed',
+    version='0.1',
+    packages=find_packages(),
+    author='MERIEUX Rudy',
+    description='Automed was created to make machine learning as simple as possible'
+)
