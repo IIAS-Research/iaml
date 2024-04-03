290c25670ee1a56c41cff10a73f8dbb8f483bfa0
Rudy Merieux <rmerieux@chu-reims.fr>
2024-04-03 16:32:33 +0200

    [ADD] Bench v2

--- src/perf_logger/bench.py
+++ src/perf_logger/bench.py
@@ -8,19 +8,16 @@ from datetime import datetime
 from os.path import exists
 import pandas as pd 
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
+from sklearn.model_selection import StratifiedKFold
 from sklearn.utils.multiclass import type_of_target
-from sklearn.metrics import balanced_accuracy_score
+
+import naiveautoml
 
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, CURRENT_PATH+"/../")
-from automed import *
+from automed.automed import *
 
-# current_duration = 30 #60*15
-# current_duration = int(sys.argv[1])
-
-def load_file(file_path:str):
+def file_to_fold(file_path:str):
     """
     Load a file and split it to X, y, X_test, y_test 
     """
@@ -32,95 +29,97 @@ def load_file(file_path:str):
     if len(df.columns) < 2:
         df = pd.read_csv(file_path, sep=",")
                 
-    labels = list(set(filter(lambda x: x[0:5] == 'label', df.columns)))
+    y = np.array(df['label'])
+    X = df.drop(columns=['label'])
     
-    train_df, test_df = train_test_split(df, test_size=0.2, random_state=43)
-    y = np.array(train_df[labels[0]])
-    X = train_df.drop(columns=labels)
-    
-    y_test = np.array(test_df[labels[0]])
-    X_test = test_df.drop(columns=labels)
+    kfold = StratifiedKFold(5)
+    for idx_train, idx_test in kfold.split(X, y):
+        X_train = X.iloc[idx_train].copy()
+        X_test = X.iloc[idx_test].copy()
+        y_train = y[idx_train].copy()
+        y_test = y[idx_test].copy()
         
-    return X, y, X_test, y_test
+        yield (X_train, y_train, X_test, y_test)
 
-def each_file(file:str, duration) -> pd.DataFrame:
+def each_file(file:str, trainer, duration) -> pd.DataFrame:
     """
     Fit and evaluate automl model on a file
     """
-    start_file = datetime.now()
     
     filename = file.split('/')[-1]
-    print('FILE :', filename)
-    X, y, X_test, y_test = load_file(file)
-
-    Cache.reset()
-    estimator = AutoMed(quiet=True, max_workers=12, max_duration=duration)
     
+    # TRAIN
+    folds_results = []
     try:
-        estimator.fit(X, y)
-        
-        end_file = datetime.now()
-        compute_time = (end_file - start_file).seconds
-        
-        y_pred = estimator.chosen_model.predict(X_test)
-        if type_of_target(y_test) == 'continuous':
-            best_result = r2_score(y_test, y_pred)
-        else:
-            best_result = balanced_accuracy_score(y_test, y_pred) 
-        
-        return_df = pd.DataFrame([
-                [duration, str(estimator.chosen_model.predictor[1].__class__), str(start_time), filename, best_result, compute_time]
-            ]
-            , columns=['duration', 'model_name', 'date', 'dataset_name','perf', 'compute_time'])
-        
-        del estimator
-    except Exception as e:
-        del estimator
-        raise Exception(f"Error occurs during bench. {e}")
+        for idx, (X, y, X_test, y_test) in enumerate(file_to_fold(file)):
+            start_fold = time.time()
+            
+            print('FILE :', filename, 'fold', idx)
+            model, model_name = trainer(X, y, duration)
+
+            # EVALUATE
+            y_pred = model.predict(X_test)
+            fold_dict = {"bench_date": start_time,
+                        "compute_time": int(time.time() - start_fold),
+                        "dataset": filename,
+                        "model_name": model_name}
+            
+            for metric_sub_class in Metric.__subclasses__():
+                metric = metric_sub_class()
+                if metric.suitable(X, y, type_of_target(y)):
+                    fold_dict[str(metric)] = metric.compute(y_test, y_pred)
+            
+            folds_results.append(fold_dict)
+    except Exception as ex:
+        print('ERROR with', filename)
+        print(ex)
+        return []
     
-    return return_df
+    return folds_results
 
 start_time = int(datetime.now().timestamp())
 
+def train_automed(X, y, duration):
+    Cache.reset()
+    estimator = AutoMed(quiet=True, max_workers=12, max_duration=duration)
+    estimator.fit(X, y)
+    return estimator.chosen_model, f"{estimator.chosen_model.transformers[-1][0]} -> {estimator.chosen_model.predictor[0]}"
+
+
+def train_naive(X, y, duration):
+    estimator = naiveautoml.NaiveAutoML(timeout=duration)
+    estimator.fit(X, y)
+    return estimator.chosen_model, f"{estimator.chosen_model}"
+
+
 def main():
     durations = [30, 120, 300, 900, 1800]
-    while True:
-        for duration in durations:
-            files = glob.glob(CURRENT_PATH+"/tests_data/*.csv")
-            dont_push_csv = glob.glob(CURRENT_PATH+"/tests_data/dont_push/*.csv")
-            files = files + dont_push_csv
+    for duration in durations:
+        files = glob.glob(CURRENT_PATH+"/tests_data/*.csv")
+        dont_push_csv = glob.glob(CURRENT_PATH+"/tests_data/dont_push/*.csv")
+        files = files + dont_push_csv
 
-            history_path = CURRENT_PATH+"/tests_data/bench_automed2_2.log"
+        history_path = CURRENT_PATH+"/tests_data/bench_v2.log"
 
-            if not exists(history_path):
-                if not exists(CURRENT_PATH+"/tests_data/"):
-                    os.mkdir(CURRENT_PATH+"/tests_data/")
-                results = pd.DataFrame(columns= \
-                    ['duration', 'model_name', 'date', 'dataset_name','perf', 'compute_time'])
-            else:
-                results = pd.read_csv(history_path)
+        if not exists(history_path):
+            if not exists(CURRENT_PATH+"/tests_data/"):
+                os.mkdir(CURRENT_PATH+"/tests_data/")
+            results = []
+        else:
+            results = pd.read_csv(history_path).to_dict('records')
 
 
-            # files = [files[5]]
-            for file in files:
-                print("->>>", file)
-                print("TIME", str(datetime.now()))
-                
-                try:
-                    output = each_file(file, duration)
-                    
-                    print("######")
-                    print("RESULT", file, output)
-                    print("######")
-                    
-                    results = pd.concat([output, results], ignore_index=True)
-                    results.to_csv(history_path, index=False)
-                except Exception as e:
-                    print(traceback.format_exc())
-                    print("FAIL : ", file)
-                    print(f"ERROR : {e}")
-            
-    print("FINISHED")
+        for file in files:
+            print(str(datetime.now()), "->>>", file)
+            try:
+                outputs = each_file(file, train_naive, duration)
+
+                results += outputs
+                pd.DataFrame(results).to_csv(history_path, index=False)
+            except Exception as e:
+                print(traceback.format_exc())
+                print("FAIL : ", file)
+                print(f"ERROR : {e}")
 
 if __name__ == "__main__":
     main()
