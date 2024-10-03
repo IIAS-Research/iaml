be1ea75b3528f49e2062ef94c5a271011e04ac57
Rudy MERIEUX <rmerieux@chu-reims.fr>
2024-10-03 16:44:00 +0000

    [ADD] Core dispatcher

--- src/iaml/iaml/core_dispatcher.py
+++ src/iaml/iaml/core_dispatcher.py
@@ -4,8 +4,11 @@
 import psutil
 import time
 import threading
+import multiprocess
 from .meta_singleton import MetaSingleton
 from .logger import Logger
+import signal
+
 
 class CoreDispatcher(metaclass=MetaSingleton):
     """
@@ -13,16 +16,16 @@ class CoreDispatcher(metaclass=MetaSingleton):
     """
     def __init__(self, *args, **kwargs) -> None:
         self.__all_cores = set(range(psutil.cpu_count(logical=True)))
-        self.__books = []
-        self.daemon = None
-        
-        
+        self.manager = multiprocess.Manager()
+        self.books = self.manager.list([])
+ 
 
-    def reset(self) -> None:
+    def reset_books(self) -> None:
         """
         Set all cpu cores as available
         """
-        self.__books = []
+        while len(self.books) > 0:
+            self.books.pop(0)
                 
     @property
     def available_cores(self) -> set[int]:
@@ -30,15 +33,10 @@ class CoreDispatcher(metaclass=MetaSingleton):
         All cores minus booked ones
         """
         avail = self.__all_cores
-        for book in self.__books:
-            avail -= book['cores']
+        for book in self.books:
+            avail -= set(book['cores'])
             
         return avail
-    
-    def __run_daemon(self):
-        if not self.daemon or not self.daemon.is_alive():
-            self.daemon = threading.Thread(target=self.__keep_running)
-            self.daemon.start()
     
     def __book_cpu(self, pids:set[int], number:int) -> None:
         """
@@ -51,17 +49,17 @@ class CoreDispatcher(metaclass=MetaSingleton):
         Raises:
             RuntimeError: Not enough CPU cores available
         """
-        
-        if len(self.available_cores) < number:
-            raise RuntimeError('Not enough CPU cores available')
-        
-        to_book = list(self.available_cores)[:number]
-        self.__books.append({
-            'pids': pids,
-            'cores': to_book
-        })
-        
-        print("BOOK !", to_book)
+        self.__free_cores()
+        with self.manager.Lock():
+            if len(self.available_cores) < number:
+                raise RuntimeError('Not enough CPU cores available')
+            
+            
+            to_book = list(self.available_cores)[:number]
+            self.books.extend([{
+                'pids': pids,
+                'cores': to_book
+                }])
         
         for pid in pids:
             process = psutil.Process(pid)
@@ -79,29 +77,24 @@ class CoreDispatcher(metaclass=MetaSingleton):
                 (each process run the same target). Defaults to 1.
 
         """
-        
         test_process = psutil.Process(pids[0])
         if hasattr(test_process, 'cpu_affinity'):
             self.__book_cpu(pids, core_number)
-            self.__run_daemon()
         else:
-            Logger().warning('Your OS doesn\'t support CPU afifnity. We are not able to control CPU cores access')
+            Logger().warning('Your OS doesn\'t support CPU affinity. We are not able to control CPU cores access')
     
-    def __keep_running(self):
+    def __free_cores(self) -> None:
         """
-        Daemon THREAD process. Infinite loop to free cpu core when process are terminated
+        Free cores that are not used anymore
         """
-        while True:
-            if not self.__books:
-                break
-
+        with self.manager.Lock():
             new_books = []
-            for book in self.__books:
-                if any(psutil.Process(pid).is_running() for pid in book['pids']):
+            for book in self.books:
+                if any(psutil.pid_exists(pid) and psutil.Process(pid).is_running() for pid in book['pids']):
                     new_books.append(book)
+                    
+            self.reset_books()
+            self.books.extend(new_books)
 
-            self.__books = new_books
-            
-            time.sleep(0.5)
-    
         
+CoreDispatcher() # Run it a first time to init shared objects
