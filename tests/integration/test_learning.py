a7d8538bbe90cbb58813bb275b72f3a111648282
Rudy Merieux <rmerieux@chu-reims.fr>
2024-10-11 10:30:35 +0200

    [TEST] Plus de tests

---
+++ tests/integration/test_learning.py
@@ -0,0 +1,105 @@
+import unittest, sys
+import pandas as pd
+import numpy as np
+
+# Include tools lib
+sys.path.append('./src/iaml')
+from iaml import *
+from iaml.explanation import Explanation
+
+class TestLearning(unittest.TestCase):
+    
+    def __learning_test(self, X, y, shap=True, name=None):
+        iaml = IAML(max_duration=60, max_workers=2)
+        outputs = iaml.fit(X, y, verbose=0)
+        
+        self.assertTrue(isinstance(outputs[0], Candidate), name)
+        self.assertTrue(outputs[0].computed_metrics is not None)
+        
+        perf_plots = iaml.chosen_candidate.explain_model_performance(X, y, X_train=X, y_train=y)
+        self.assertTrue(isinstance(perf_plots[0], Plot), name)
+        
+        if shap:
+            exp = iaml.chosen_model.explain_model(X.sample(40))
+            shap_plots = exp.to_plots()
+            self.assertTrue(isinstance(exp, Explanation), name)
+            self.assertTrue(isinstance(shap_plots[0], Plot), name)
+            
+    
+    def load_and_sample(self, path, size=1000):
+        df = None
+        try:
+            df = pd.read_csv(path, sep=";")
+        except:
+            df = pd.DataFrame()
+            
+        if len(df.columns) < 2:
+            df = pd.read_csv(path, sep=",")
+            
+        if df.shape[0] > size:
+            df = df.sample(size)
+            
+        return df
+            
+                
+
+    def test_regression(self):
+        """
+        Test : Regression on a CSV file
+        """
+        datasets = ['./src/perf_logger/tests_data/insurance.csv',
+                    './src/perf_logger/tests_data/life_expectancy.csv',
+                    './src/perf_logger/tests_data/concrete_data.csv',
+                    './src/perf_logger/tests_data/ds_salaries.csv',
+                    './src/perf_logger/tests_data/tips.csv'
+                    ]
+        
+        for dataset in datasets:
+            df = self.load_and_sample(dataset)
+            y = df['label']
+            X = df.drop(columns=['label'])
+                
+            self.__learning_test(X, y, name=dataset)
+    
+    
+    def test_classification(self):
+        """
+        Test : Classification on a CSV file
+        """
+        datasets = ['./src/perf_logger/tests_data/body_signal_of_smoking.csv',
+                    './src/perf_logger/tests_data/bbc-text.csv',
+                    './src/perf_logger/tests_data/Drug_classification.csv',
+                    './src/perf_logger/tests_data/fertility.csv',
+                    './src/perf_logger/tests_data/fetal_health.csv',
+                    './src/perf_logger/tests_data/healthcare-dataset-stroke-data.csv',
+                    './src/perf_logger/tests_data/HepatitisCdata.csv',
+                    './src/perf_logger/tests_data/titanic.csv'
+                    ]
+        
+        for dataset in datasets:
+            df = self.load_and_sample(dataset)
+            y = df['label']
+            X = df.drop(columns=['label'])
+                
+            self.__learning_test(X, y, name=dataset)
+
+    def test_survival(self):
+        """
+        Test : Survival on a CSV file
+        """
+        datasets = [
+                    './src/perf_logger/tests_data/seer.csv',
+                    './src/perf_logger/tests_data/chc.csv'
+                    ]
+        
+        for dataset in datasets:
+            df = self.load_and_sample(dataset)
+        
+            # df['event'] = df['event'].replace({'Alive': False, 'Dead': True})
+            df['label'] = list(zip(df['event'], df['event_time']))
+            df.drop(columns=['event', 'event_time'], inplace=True)
+        
+            y = df['label']
+            X = df.drop(columns=['label'])
+
+            self.__learning_test(X, y, shap=False)
---
+++ tests/unit/test_learning_fast.py
@@ -0,0 +1,64 @@
+import unittest, sys
+import pandas as pd
+import numpy as np
+
+# Include tools lib
+sys.path.append('./src/iaml')
+from iaml import *
+from iaml.explanation import Explanation
+
+class TestLearningFast(unittest.TestCase):
+    
+    def __learning_test(self, X, y, shap=True):
+        iaml = IAML(max_duration=60, max_workers=2)
+        outputs = iaml.fit(X, y, verbose=2)
+        
+        self.assertTrue(isinstance(outputs[0], Candidate))
+        self.assertTrue(outputs[0].computed_metrics is not None)
+        
+        perf_plots = iaml.chosen_candidate.explain_model_performance(X, y, X_train=X, y_train=y)
+        self.assertTrue(isinstance(perf_plots[0], Plot))
+        
+        if shap:
+            exp = iaml.chosen_model.explain_model(X.sample(40))
+            shap_plots = exp.to_plots()
+            self.assertTrue(isinstance(exp, Explanation))
+            self.assertTrue(isinstance(shap_plots[0], Plot))
+                
+
+    def test_regression(self):
+        """
+        Test : Fast regression on a CSV file
+        """
+        df = pd.read_csv('./src/perf_logger/tests_data/life_expectancy.csv', sep=",")
+        y = df['label']
+        X = df.drop(columns=['label'])
+            
+        self.__learning_test(X, y)
+    
+    
+    def test_classification(self):
+        """
+        Test : Fast classification on a CSV file
+        """
+        df = pd.read_csv('./src/perf_logger/tests_data/fertility.csv', sep=",")
+        
+        y = df['label']
+        X = df.drop(columns=['label'])
+            
+        self.__learning_test(X, y)
+
+    def test_survival(self):
+        """
+        Test : Fast survival on a CSV file
+        """
+        df = pd.read_csv('./src/perf_logger/tests_data/seer.csv', sep=",")
+        
+        df['Status'] = df['Status'].replace({'Alive': False, 'Dead': True})
+        df['label'] = list(zip(df['Status'], df['Survival Months']))
+        df.drop(columns=['Status', 'Survival Months'], inplace=True)
+        
+        y = df['label']
+        X = df.drop(columns=['label'])
+
+        self.__learning_test(X, y, shap=False)
