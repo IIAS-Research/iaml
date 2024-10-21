c683757f0bce8fced5c7e50b2b48a040b25f5888
Hugo Ruellet <hruellet@chu-reims.fr>
2024-10-21 13:51:04 +0000

    [ADD] New precleaning actionnables, trimming columns names AND str/object columns. It appears that spaces are not a problem for numerical column as it automatically detect the good type

--- src/iaml/iaml/actionables/features_precleaning/__init__.py
+++ src/iaml/iaml/actionables/features_precleaning/__init__.py
@@ -4,3 +4,4 @@ Example : Convert SHORT_TEXT column into datetime if possible
 """
 from .act_date_converter import ActDateConverter
 from .act_drop_bad_quality_rows import ActDropBadQualityRows
+from .act_trim_space import ActTrimSpaces
