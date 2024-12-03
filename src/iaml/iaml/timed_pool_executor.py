58044122118ed89c08cb2b06ef07e1f4458e7d47
Hugo Ruellet <hruellet@chu-reims.fr>
2024-12-03 13:37:26 +0000

    More lint

--- src/iaml/iaml/timed_pool_executor.py
+++ src/iaml/iaml/timed_pool_executor.py
@@ -1,14 +1,11 @@
+"""TimedPoolExecutor will run *max_workers* new process and will send them actions
+to run.
+Compare to ProcessPoolExecutor, this one allow us to kill process quickly after timeout
 """
-    TimedPoolExecutor will run *max_workers* new process and will send them actions
-    to run.
-    Compare to ProcessPoolExecutor, this one allow us to kill process quickly after timeout
-"""
-
 import random
 import signal
 import time
 import traceback
-from typing import Any, List
 import warnings
 import threading
 import multiprocess
@@ -39,14 +36,14 @@ def process_daemon(
         Used to count number of ran actions
     """
     result = None
-    
+
     time.sleep(random.random()) # Weird thing to un-sync the threads
-    
+
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore")
         while True:
             value = to_run_queue.get()
-            
+
             method, args, kwargs, callback_id = value
             try:
                 result = method(*args, **kwargs)
@@ -55,7 +52,7 @@ def process_daemon(
                 error_queue.put((traceback.format_exc(), callback_id))
             finally:
                 finally_queue.put(1)
-            
+
 
 class TimedPoolExecutor:  # pylint: disable=too-many-instance-attributes
     """TimedPoolExecutor will run *max_workers* new process and will send them actions
@@ -87,11 +84,11 @@ class TimedPoolExecutor:  # pylint: disable=too-many-instance-attributes
         self.sliding_stages: bool = sliding_stages
         """If True, don't wait for all workers to end, leaving empty cpu cores"""
 
-        # Daemon THREAD (& not Process) 
+        # Daemon THREAD (& not Process)
         self.main_daemon: threading.Thread = None
         """Main runnng thread with a infinite loop to catch results of sub process"""
 
-        self.daemons_collectors: List[threading.Thread] = None
+        self.daemons_collectors: list[threading.Thread] = None
         """List of running daemons"""
 
         self.manager: multiprocess.Manager = multiprocess.Manager()
@@ -99,7 +96,7 @@ class TimedPoolExecutor:  # pylint: disable=too-many-instance-attributes
 
         self.to_run_queue: multiprocess.Manager.Queue = self.manager.Queue()
         """Queue used to exchange data with sub process"""
-        
+
         self.error_queue: multiprocess.Manager.Queue  = self.manager.Queue()
         """Queue used to exchange data with sub process"""
 
@@ -109,10 +106,10 @@ class TimedPoolExecutor:  # pylint: disable=too-many-instance-attributes
         self.finally_queue: multiprocess.Manager.Queue  = self.manager.Queue()
         """Queue used to exchange data with sub process"""
 
-        self.callbacks: List[callable] = [callback]
+        self.callbacks: list[callable] = [callback]
         """Method to call after each run"""
 
-        self.results: List = []
+        self.results: list = []
         """List of all result since last reset"""
 
         self.submit_count: int = 0
@@ -121,9 +118,9 @@ class TimedPoolExecutor:  # pylint: disable=too-many-instance-attributes
         self.finished_run: int = 0
         """Count -> Help TimedPoolExecutor to know if everything is finished"""
 
-        self.process: List[multiprocess.Process] = []
+        self.process: list[multiprocess.Process] = []
         """List of sub processes"""
-        
+
         # Create and start sub process (will only wait until first submit)
         for _ in range(max_workers):
             self.process.append(
@@ -137,22 +134,22 @@ class TimedPoolExecutor:  # pylint: disable=too-many-instance-attributes
                 )
             )
             self.process[-1].start()
-            
+
         CoreDispatcher().affiliate(
             [process.pid for process in self.process],
             core_number=self.max_workers)
-            
+
         self.__run_daemon() # Run the daemon THREAD
 
         if threading.current_thread() is threading.main_thread():
             signal.signal(signal.SIGINT, lambda *_: self.shutdown())
             signal.signal(signal.SIGTERM, lambda *_: self.shutdown())
-        
+
     def __del__(self):
         """When delete -> TimedPoolExecutor kill all these daemons
         """
         self.shutdown()
-    
+
     def shutdown(self) -> None:
         """Shutdown TimedPoolExecutor : Kill subprocess and thread
         """
@@ -177,23 +174,20 @@ class TimedPoolExecutor:  # pylint: disable=too-many-instance-attributes
     def __collect_finally(self) -> None:
         while self.finally_queue.get() != "stop":
             self.finished_run += 1
-        
-        
+
     def __print_errors(self) -> None:
-        """Collect and print error from error_queue
-        """
+        """Collect and print error from error_queue"""
         while True:
             error, callback_id = self.error_queue.get()
-            
+
             if error == 'stop':
                 break
-            
+
             Logger().error("Error in a subprocess : ", error)
             self.callbacks[callback_id](None)
-    
+
     def __keep_running(self) -> None:
-        """Daemon THREAD process. Infinite loop to catch results & errors
-        """
+        """Daemon THREAD process. Infinite loop to catch results & errors"""
         while True:
             if self.stop_flag:
                 self.error_queue.put(("stop", None)) # Gentilly ask thread to stop
@@ -208,34 +202,31 @@ class TimedPoolExecutor:  # pylint: disable=too-many-instance-attributes
                     self.to_run_queue.get()
 
                 self.manager.shutdown()
-                
+
                 break
 
             Logger().print_queue()
             time.sleep(0.5)
-    
+
     def __run_daemon(self) -> None:
-        """Start the daemon THREAD
-        """
+        """Start the daemon THREAD"""
         self.stop_flag = False
         if not self.main_daemon or not self.main_daemon.is_alive():
             self.main_daemon = threading.Thread(target=self.__keep_running)
             self.main_daemon.start()
-            
+
             self.__run_collectors()
-            
+
     def __run_collectors(self) -> None:
-        """Start collector daemons
-        """
+        """Start collector daemons"""
         self.daemons_collectors = [
             threading.Thread(target=self.__print_errors),
             threading.Thread(target=self.__collect_results),
             threading.Thread(target=self.__collect_finally)]
-        
+
         for collector in self.daemons_collectors:
             collector.start()
-                
-        
+
     def submit(self, target: callable, *args, **kwargs) -> None:
         """Submit a new task to sub process
 
@@ -252,14 +243,14 @@ class TimedPoolExecutor:  # pylint: disable=too-many-instance-attributes
         else:
             self.to_run_queue.put((target, args, kwargs, len(self.callbacks)-1))
             self.submit_count += 1
-        
+
     def __finished(self) -> bool:
         """Are all the submitted tasks finished?
         
         :return: True if all tasks are finished
         """
         return self.finished_run >= self.submit_count
-    
+
     def reset(self):
         """Reset all queues, callback, results, etc. 
         Allow to reuse this instance of TimedPoolExecutor without restarting subProcess
@@ -268,34 +259,33 @@ class TimedPoolExecutor:  # pylint: disable=too-many-instance-attributes
             self.callbacks = [self.callbacks[-1]]
             self.submit_count = 0
             self.finished_run = 0
-            
+
         self.results = []
-        
+
     def __join_collectors(self):
         """Join collector thread.
         Stop and start thread, used when we want to sync with thread to collect all data 
         """
-        
+
         # Stop and join collector
         self.error_queue.put(("stop", None))
         self.result_queue.put(("stop", None))
         self.finally_queue.put("stop")
-        
+
         for collector in self.daemons_collectors:
             collector.join()
-            
+
         # Restart collectors
         self.__run_collectors()
-        
-                
+
     def set_callback(self, callback: callable) -> None:
         """Set the method call to when a task finish
 
         :param callable callback: callback method
         """
         self.callbacks.append(callback)
-        
-    def join(self, timeout: int, reset: bool = True) -> List:
+
+    def join(self, timeout: int, reset: bool = True) -> list:
         """Wait until all the task are finished or timeout is reach
         If timeout is reach -> Remaining tasks will be kill without sending results
 
@@ -307,7 +297,7 @@ class TimedPoolExecutor:  # pylint: disable=too-many-instance-attributes
         start_time = time.monotonic()
         def remain_time():
             return max(0, int(timeout - (time.monotonic() - start_time)))
-        
+
         def slide():
             try:
                 is_empty = self.to_run_queue.empty()
@@ -315,7 +305,7 @@ class TimedPoolExecutor:  # pylint: disable=too-many-instance-attributes
                 is_empty = True
 
             return self.sliding_stages \
-                and ( 
+                and (
                     is_empty # submit queue is empty
                     and (
                         self.submit_count - self.finished_run <= self.max_workers/2
@@ -323,16 +313,16 @@ class TimedPoolExecutor:  # pylint: disable=too-many-instance-attributes
                         )
                     and self.results # We have got at least one result
                 )
-        
+
         while not self.__finished() and remain_time() and not slide():
             time.sleep(0.5)
-            
+
         # Join collector thread, just to be sure we have collected all data
         self.__join_collectors()
-        
+
         results = self.results # Save before reset!
-        
+
         if reset:
             self.reset()
-        
+
         return results
