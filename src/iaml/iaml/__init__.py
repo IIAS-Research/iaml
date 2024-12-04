522c8a04c81056d8e3be42ddf1915dd0b82f8434
Rudy Merieux <rmerieux@chu-reims.fr>
2024-12-04 10:31:22 +0100

    Main docstring

--- src/iaml/iaml/__init__.py
+++ src/iaml/iaml/__init__.py
@@ -1,5 +1,12 @@
 """
-    IAML is an autoML tools focusing on Medical Dataset with explainable models  
+    IAML (Incremental AutoML), a high-performance and modular,
+    open-source Python framework. Designed to mimics the behavior
+    of a data scientist in creating pipelines and leverages an 
+    optimization process inspired by genetic algorithm for
+    efficient pipeline construction and hyperparameter tuning.
+    
+    The framework incorporates explainability features, such as 
+    SHAP-based insights, to enhance model transparency and trustworthiness.
 """
 from .iaml import IAML
 from .core_dispatcher import CoreDispatcher
