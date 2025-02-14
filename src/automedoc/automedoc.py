b3487105d0677c2491299780ef207b317abd9e48
Rudy Merieux <rmerieux@chu-reims.fr>
2025-02-14 10:51:52 +0100

    Remove all old things

--- src/automedoc/automedoc.py
+++
@@ -1,6 +0,0 @@
-
-import sys
-sys.path.insert(0, '../python_project/src/')
-from iaml.iaml import *
-
-from .act_atc_code import ActAtcCode
--- src/scripts/__main__.py
+++
@@ -1,6 +0,0 @@
-def main():
-    # Votre super code ici..
-
-
-if __name__ == '__main__':
-    main()
