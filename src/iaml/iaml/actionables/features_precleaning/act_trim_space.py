fea58010369091256e15f3600164696ad834659c
Robin BOURACHOT <rbourachot@chu-reims.fr>
2025-02-19 09:19:28 +0000

    [FIX] ActTrimSpace altering dtypes (#54)

--- src/iaml/iaml/actionables/features_precleaning/act_trim_space.py
+++ src/iaml/iaml/actionables/features_precleaning/act_trim_space.py
@@ -49,13 +49,14 @@ class ActTrimSpaces(Actionable):
             X = X.rename(columns=lambda x: x.lstrip())
             for col in X.columns:
                 if isinstance(X[col].dtype, str) or is_object_dtype(X[col]):
-                    X[col] = X[col].apply(lambda x: x.lstrip() if isinstance(x, str) else x)
+                    X[col] = X[col].apply(lambda x: x.lstrip() if isinstance(x, str) else x, convert_dtype=False)
 
         if self.get_config('right_trim'):
             X = X.rename(columns=lambda x: x.rstrip())
             for col in X.columns:
                 if isinstance(X[col].dtype, str) or is_object_dtype(X[col]):
-                    X[col] = X[col].apply(lambda x: x.rstrip() if isinstance(x, str) else x)
+                    X[col] = X[col].apply(lambda x: x.rstrip() if isinstance(x, str) else x, convert_dtype=False)
+
         return X
 
     def priorize(self, candidate: Candidate = None) -> float:
--- src/iaml/iaml/iaml_pipeline.py
+++ src/iaml/iaml/iaml_pipeline.py
@@ -457,11 +457,13 @@ class IAMLPipeline(Pipeline):
             raise RuntimeError('There is no model to explain.')
 
         def p(pred_data):
-            if hasattr(self, 'predict_proba'):
-                return self.predict_proba(pd.DataFrame(pred_data, columns=X.columns))[:, 1]
+            df = pd.DataFrame(pred_data, columns=X.columns)
 
-            # Regressor does not implement predict_proba
-            return self.predict(pd.DataFrame(pred_data, columns=X.columns))
+            if hasattr(self, 'predict_proba'):
+                return self.predict_proba(df)[:, 1]
+
+            # when the regressor does not implement predict_proba
+            return self.predict(df)
 
         mask_dataset = self.original_dataset if self.original_dataset is not None \
             and not self.original_dataset.empty else X
